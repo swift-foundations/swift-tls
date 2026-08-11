bf58a95a2a4cf70078075237d8d6b72b5e367eba
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-11 09:50:56 +0200

    Rebind TLS to Byte Channel lifetime views

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/swift-foundations/swift-certificate-verification.git", revision: "1b4fc68"),
         .package(
             url: "https://github.com/swift-foundations/swift-byte-channel.git",
-            revision: "d2bed89fb434962fe055351e0d6dfaaf5e2a1d68"
+            revision: "f56b4393496fd52fffd1f27bfffca3b101a992d2"
         ),
         .package(url: "https://github.com/swift-primitives/swift-span-primitives.git", branch: "main"),
     ],
