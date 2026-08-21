a2f56478b147c39d39215643f9ce422ad6dc9fbc
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 13:21:27 +0200

    Remove LifetimeDependence feature flags
    Remove .enableExperimentalFeature("LifetimeDependence") and .enableUpcomingFeature("LifetimeDependence").

--- Package.swift
+++ Package.swift
@@ -63,8 +63,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
             .enableUpcomingFeature("InternalImportsByDefault"),
             .enableUpcomingFeature("MemberImportVisibility"),
             .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
-            .enableExperimentalFeature("LifetimeDependence"),
-            .enableExperimentalFeature("Lifetimes"),
+                .enableExperimentalFeature("Lifetimes"),
                 .enableUpcomingFeature("InferIsolatedConformances"),
         ]
 }
